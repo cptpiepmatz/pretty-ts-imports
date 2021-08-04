4d218b52dda2e27d44669156d620b2fb4e39daa2
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-08-04 17:21:54 +0200

    added more compare functions

---
+++ src/compareFunctions/Imports/compareSources.ts
@@ -0,0 +1,16 @@
+import ImportCompareFunction from "../ImportCompareFunction";
+import Import from "../../Import";
+
+/**
+ * Simply compares the source names.
+ * @param importA Import A
+ * @param importB Import B
+ */
+const compareSources: ImportCompareFunction = function(
+  importA: Import,
+  importB: Import
+): number {
+  return importA.source.name.localeCompare(importB.source.name);
+}
+
+export default compareSources;
