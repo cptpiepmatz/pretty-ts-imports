b84ce205e81413d1f5f03220b5678082f1d0ddfa
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-08-05 21:17:37 +0200

    Added some examples

---
+++ examples/compare_functions/imports/dotJSFirst.ts
@@ -0,0 +1,20 @@
+import ImportCompareFunction from "../../../src/compare_functions/ImportCompareFunction";
+import Import from "../../../src/import_management/Import";
+
+/**
+ * Example compare function for imports.
+ * This does put imports with the source that end with ".js" first.
+ * @param importA Import A
+ * @param importB Import B
+ */
+const dotJSFirst: ImportCompareFunction = function(
+  importA: Import,
+  importB: Import
+): number {
+  const matcher = /\.[Jj][Ss]$/;
+  const matchesA = !!importA.source.name.match(matcher);
+  const matchesB = !!importB.source.name.match(matcher);
+  if (importA.source.isPackage && matchesA && !matchesB) return -1;
+  if (importB.source.isPackage && !matchesA && matchesB) return 1;
+  return 0;
+}
