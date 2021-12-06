73ded3a6cbf7c25c169b7bbff868b3e628cae1b6
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-12-06 21:34:09 +0100

    added an on demand transpiler class

---
+++ src/config/OnDemandTranspiler.ts
@@ -0,0 +1,43 @@
+import {dirname, join, resolve} from "path";
+import {readFileSync} from "fs";
+import {parse} from "json5";
+import {CompilerOptions} from "typescript";
+import ImportCompareFunction from "../sort_rules/ImportCompareFunction";
+import ImportElementCompareFunction
+  from "../sort_rules/ImportElementCompareFunction";
+import SeparateByFunction from "../sort_rules/SeparateByFunction";
+import {transpile as tsTranspile} from "typescript";
+import requireFromString from "require-from-string";
+
+type RequiredFunction =
+  ImportCompareFunction | ImportElementCompareFunction | SeparateByFunction;
+
+export default class OnDemandTranspiler {
+
+  // TODO: doc me
+  // TODO: test me
+
+  private readonly compilerOptions: CompilerOptions;
+  private readonly configPath: string | undefined;
+
+  constructor(tsConfigPath: string, configPath: string | undefined) {
+    this.configPath = configPath;
+    let tsConfigContent = readFileSync(
+      join(__dirname, "../../tsconfig.json"),
+      "utf8"
+    );
+    let tsConfigObject = parse(tsConfigContent);
+    this.compilerOptions = tsConfigObject.compilerOptions as any;
+  }
+
+  transpile(sourcePath: string): RequiredFunction {
+    // TODO: add some more detection if something is wrong
+    if (!this.configPath) throw new Error("No config path given");
+    let requirePath = resolve(dirname(this.configPath), sourcePath);
+    let requireContent = readFileSync(requirePath, "utf-8");
+    let transpiled = tsTranspile(requireContent, this.compilerOptions);
+    let required = requireFromString(transpiled);
+    return required.default;
+  }
+
+}
