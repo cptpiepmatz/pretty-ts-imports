10faa34603019fa1a3c9ce731b0ae9ebdea7447b
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-12-09 23:54:34 +0100

    Integrated the OnDemandTranspiler

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import ConfigHandler from "./config/ConfigHandler";
 import ImportSorter from "./import_management/ImportSorter";
 import ImportSeparator from "./import_management/ImportSeparator";
 import ImportIntegrator from "./import_management/ImportIntegrator";
+import OnDemandTranspiler, {RequiredFunction} from "./config/OnDemandTranspiler";
 
 const cliHandler = new CLIHandler();
 const {givenFileOrDirPath, shallRecursive, primpConfigPath} = cliHandler;
@@ -12,8 +13,15 @@ const tsConfigPath = cliHandler.tsConfigPath ?? process.cwd();
 const fileManager = new FileManager(tsConfigPath, files);
 const configPath = primpConfigPath ?? ConfigHandler.findConfig(givenFileOrDirPath);
 const configHandler = new ConfigHandler(configPath);
+const requiredRecord: Record<string, RequiredFunction> = {};
+if (configPath) {
+  const onDemandTranspiler = new OnDemandTranspiler(tsConfigPath, configPath);
+  for (const [name, path] of Object.entries(configHandler.require)) {
+    requiredRecord[name] = onDemandTranspiler.transpile(path);
+  }
+}
 const {sortImports, sortImportElements} = configHandler;
-const sorter = new ImportSorter(sortImports, sortImportElements);
+const sorter = new ImportSorter(sortImports, sortImportElements, requiredRecord);
 const separator = new ImportSeparator(configHandler.separateBy);
 const integrator = new ImportIntegrator(configHandler.formatting);
 for (let imported of fileManager.imports) {
