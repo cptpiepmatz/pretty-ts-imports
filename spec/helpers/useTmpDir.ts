20e95aaaf8e989015fd5074dcc1f7d2a7f2c95e5
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-12-21 14:05:08 +0100

    added a test helper function for temporary directories

---
+++ spec/helpers/useTmpDir.ts
@@ -0,0 +1,16 @@
+import {mkdirSync, mkdtempSync, rmdirSync} from "fs";
+import {join} from "path";
+
+/**
+ * Helper function to generate a tmp directory for testing.
+ *
+ * The temporary directory will be deleted after this is done.
+ * @param whileTmpDir Function to pass while the temporary directory exists
+ */
+export default function useTmpDir(whileTmpDir: (path: string) => void) {
+  const genericTmpPath = join(__dirname, "../tmp");
+  mkdirSync(genericTmpPath, {recursive: true});
+  let tmpPath = mkdtempSync(join(genericTmpPath, "/"));
+  whileTmpDir(tmpPath);
+  rmdirSync(tmpPath);
+}
