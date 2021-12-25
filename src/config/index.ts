f54ffb484a34f469f6c97734a7b84d20357b4521
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-12-25 10:41:27 +0100

    added RequiredFunction to the lib

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ export {default as Config} from "./Config";
 export {default as ConfigHandler, expectedConfigNames} from "./ConfigHandler";
 export {default as defaultConfig} from "./defaultConfig";
 export {default as FullConfig} from "./FullConfig";
-export {default as OnDemandTranspiler} from "./OnDemandTranspiler";
+export {default as OnDemandTranspiler, RequiredFunction} from "./OnDemandTranspiler";
 export {default as SupportedConfigFormat} from "./SupportedConfigFormat";
